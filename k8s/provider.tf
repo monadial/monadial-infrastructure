7649c08bc52bffa7caf319c337783f89ae9f25ba
Tomas Mihalicka <tomas@mihalicka.com>
2021-05-06 17:56:51 +0200

    Added TF_VARS for env variables

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -1,14 +1,17 @@
 variable "scaleway_access_key" {
-  type        = string
+  type = string
   description = "Scaleway access key."
 }
 
 variable "scaleway_secret_key" {
-  type        = string
+    type = string
   description = "Scaleway secret key."
 }
 
 provider "scaleway" {
   access_key = var.scaleway_access_key
-  secret_key = var.scaleway_secret_key
+  secret_key =  var.scaleway_secret_key
+  project_id = "default"
+  region = "nl-ams"
+  zone = "nl-ams-1"
 }
