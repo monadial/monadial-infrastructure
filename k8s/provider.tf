a26afbce74091bf37c80702d300cc4d1f26969b1
Tomas Mihalicka <tomas@mihalicka.com>
2021-05-06 18:01:45 +0200

    Added project_id as parameter to scaleway resource.

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -8,10 +8,15 @@ variable "scaleway_secret_key" {
   description = "Scaleway secret key."
 }
 
+variable "scaleway_project_id" {
+  type        = string
+  description = "Scaleway project id."
+}
+
 provider "scaleway" {
   access_key = var.scaleway_access_key
   secret_key = var.scaleway_secret_key
-  project_id = "default"
+  project_id = var.scaleway_project_id
   region     = "nl-ams"
   zone       = "nl-ams-1"
 }
