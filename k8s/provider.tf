c845d2d1d7285ad80f2d6dae87c5057a273a57ab
Tomas Mihalicka <tomas@mihalicka.com>
2021-05-06 11:01:33 +0200

    Monadial IAC github action

---
+++ k8s/provider.tf
@@ -0,0 +1,14 @@
+variable "scaleway_access_key" {
+  type = string
+  description = "Scaleway access key."
+}
+
+variable "scaleway_secret_key" {
+    type = string
+  description = "Scaleway secret key."
+}
+
+provider "scaleway" {
+  access_key = var.scaleway_access_key
+  secret_key =  var.scaleway_secret_key
+}
---
+++ k8s/terraform.tfvars
@@ -0,0 +1,4 @@
+# Backend variables
+# https://console.scaleway.com/project/credentials
+scaleway_access_key = "<your-scaleway-access-key>"
+scaleway_secret_key = "<your-scaleway-secret-key>"
