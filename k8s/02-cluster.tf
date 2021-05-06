ac484029392202eb7f13152f13b83f8e3a5adca8
Tomas Mihalicka <tomas@mihalicka.com>
2021-05-06 17:45:01 +0200

    Added k8s cluster resource

--- k8s/02-cluster.tf
+++ k8s/02-cluster.tf
@@ -1,23 +1,23 @@
 resource "scaleway_k8s_cluster" "monadial_k8s_cluster" {
-    name = "monadial-k8s-cluster"
-    description = "Monadial K8S production cluster"
-    version = "1.21.0"
-    cni = "cilium"
+  name        = "monadial-k8s-cluster"
+  description = "Monadial K8S production cluster"
+  version     = "1.21.0"
+  cni         = "cilium"
 
-    autoscaler_config {
-      scale_down_delay_after_add = "5m"
-      ignore_daemonsets_utilization = true
-      balance_similar_node_groups = true
-    }
+  autoscaler_config {
+    scale_down_delay_after_add    = "5m"
+    ignore_daemonsets_utilization = true
+    balance_similar_node_groups   = true
+  }
 }
 
 resource "scaleway_k8s_pool" "monadial_k8s_pool" {
-    cluster_id = scaleway_k8s_cluster.monadial_k8s_cluster.id
-    name = "monadial-k8s-pool"
-    node_type = "DEV1-M"
-    size = 2
-    autohealing = true
-    autoscaling = true
-    min_size = 2
-    max_size = 6
+  cluster_id  = scaleway_k8s_cluster.monadial_k8s_cluster.id
+  name        = "monadial-k8s-pool"
+  node_type   = "DEV1-M"
+  size        = 2
+  autohealing = true
+  autoscaling = true
+  min_size    = 2
+  max_size    = 6
 }
